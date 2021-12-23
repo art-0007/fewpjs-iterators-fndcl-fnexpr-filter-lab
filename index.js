974d45ae446d28935d459e47788aa3d8095bc88e
Artur <info@aigroupusa.com>
2021-12-23 12:11:00 -0500

    commit tree 87b184cadf049db0b4eba8b380766d9f19cffec2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, name) {
+ let result = drivers.filter(n => {
+        return n.toLowerCase() === name.toLowerCase();
+      }); 
+
+      return result
+};
+
+function fuzzyMatch(drivers, char) {
+    return drivers.filter(name => {
+        if (char.charAt(0) === name.charAt(0)){
+            return name
+        }
+    })          
+   };
+
+function matchName(drivers, name) {
+    return drivers.filter(finded_name => {
+        if (finded_name.name === name){
+            return finded_name
+        }
+    })          
+};
